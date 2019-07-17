2d6eecdd9a21fed2d82b7c0a7ecb55453ab4cb17
Tim <TAbdiukov@gmail.com>
2019-07-18 08:10:05 +1000

    Fix: no way to install PIL onto Ubuntu

--- setup.py
+++ setup.py
@@ -13,13 +13,16 @@ except ImportError:
     from distutils.core import setup
 
 
-required = ['requests>=1.0.0', 'impacket>=0.9.0', 'slackclient', 'progressbar', 'numpy', 'PIL', 'pytube', 'hashlib',
+required = ['requests>=1.0.0', 'impacket>=0.9.0', 'slackclient', 'progressbar', 'numpy', 'Pillow', 'pytube', 'hashlib',
             'urllib2', 'PyCrypto', 'ftplib', 'base58']
 			
 			# note: attempting to install "zlib" breaks "pip install", as zlib is not a pip, but a system package
 			# proof: https://stackoverflow.com/a/6169902
 			
-			
+			# note: PIL is (sadly) dead, and as part of it, there is no weay to install PIL onto Ubuntu
+			# but the workaround is simple! Just replace "PIL" with "Pillow"
+			# and luckily, no need to modify import for this particular project
+			# proof: https://stackoverflow.com/a/41326447
 			
             # Todo: Set that urllib2 is not installed from pip for Python3
 
